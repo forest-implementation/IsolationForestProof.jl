9df2bb72f90abd68a8a997fc8d7009b370f04dc4
Adam Ulrich <a_ulrich@utb.cz>
2024-03-27 16:10:25 +0100

    vraci moc zanorene

--- src/dukaz.jl
+++ src/dukaz.jl
@@ -1,5 +1,33 @@
 module dukaz
+using Lazy
 
-# Write your package code here.
+export Node, add, interval_prob, prob, interval_prob_rec
+
+add(x,y) = x + y
+
+struct Node
+    data
+    prob
+    depth
+end
+
+prob(node::Node, tups, point) = Node(
+    tups[2][1] <= point ? tups[2] : tups[1], 
+    node.prob * (tups[2][1]-last(tups[1]))/(last(node.data)-node.data[1]),
+    node.depth+1
+)
+
+interval_prob(node::Node, point) = 
+    length(node.data) == 1 ? node : @>>begin
+    range(2,length(node.data)) 
+    map(x->(node.data[1:x-1],node.data[x:length(node.data)])) 
+    map(tup -> interval_prob(prob(node,tup, point), point))
+end
+
+interval_prob_rec(nodes, point) = begin
+    length(node.data) == 1 ? node : begin
+      map(node -> interval_prob_rec(node,point), interval_prob(node, point))
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "dukaz.jl" begin
     # Write your tests here.
+
+    @test add(5,5) == 10
+    @test interval_prob(Node((15, 20, 25, 30, 35, 85, 90, 95, 105),1, 0),25) == 0
 end
